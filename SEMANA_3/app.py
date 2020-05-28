133061c02b2de35286d10cc95d67e7c2684d26eb
lupitaesp <1718110388@utectulancingo.edu.mx>
2020-05-28 01:07:15 +0000

    importacion

--- SEMANA_3/app.py
+++ SEMANA_3/app.py
@@ -1,15 +1,11 @@
 import web
 
 urls = (
-    '/(.*)', 'hello'
+    '/(.*)', 'application.controllers.cook.'
 )
 app = web.application(urls, globals())
 
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+
 
 if __name__ == "__main__":
     app.run()
